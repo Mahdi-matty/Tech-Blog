8f95d372a8c492a25a9bea12090302a995fe1d93
mahdi <mmiq69@gmail.com>
2024-01-24 15:41:28 -0500

    added byjs

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const {User,BlogPost, Comment} = require("../models");
 const userRoutes = require("./userRoutes");
 const BlogPostRoutes = require("./blogpostRoutes")
